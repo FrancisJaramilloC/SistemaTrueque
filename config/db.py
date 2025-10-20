623121500ad078824b29adf3d836625ed1ad4779
PanchoJc <francisco.jaramillo@unl.edu.ec>
2025-10-19 23:00:27 -0500

    Limpieza del main

--- config/db.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-
-engine = create_engine("mariadb+mariadbconnector://root:1234@localhost:3307/sistema_trueque") 
-
-meta_data = MetaData()
-conn = engine.connect() #Mantiene la conexion abierta
