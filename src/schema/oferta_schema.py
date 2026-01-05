e33c2bdd84b3834d64a330aaf2ca50deee56da2a
JOSERiofrio2002 <jose.f.riofrio@unl.edu.ec>
2026-01-04 21:09:56 -0500

    Actualizacion

--- src/schema/oferta_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class OfertaSchema(BaseModel):
-    id: int | None = None
-    estado: bool
-    fecha: str
-    persona_id: int  # Llave foranea
-    articulo_id: int  # Llave foranea
