224dfb66e56fb3dda711e34510f59f4280380995
PanchoJc <francisco.jaramillo@unl.edu.ec>
2026-01-08 12:00:49 -0500

    Limpieza del develop

--- src/schema/categoria_schema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class CategoriaSchema(BaseModel):
-    id: int | None = None
-    descripcion: str
--- src/schema/cliente_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class ClienteSchema(BaseModel):
-    email: str
-    username: str
-    contrasena: str
-    estado: bool
-    persona_id: int #Llave foranea
